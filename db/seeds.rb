eb3853c6fa675b5e3cceeed9417cee2ee03e4850
pcbmaster <fitnessnonsense@gmail.com>
2018-09-06 14:37:29 -0600

    seeds.rb no longer adds duplicates and adds to top-100

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,16 @@
 require 'json'
 
+@top = Topboard.create(name: "Top-100")
+
 file = File.read('song_data.json')
 song_data = JSON.parse(file)
 song_data.each do |song|
-  skip = false
-  Artist.all.each{ |a| skip = a.name == song["song_artist"] } 
-  artist = skip ? Artist.find_by!(name: song["song_artist"]) : Artist.create(name: song["song_artist"])
-  Song.create(name: song["song_title"], artist_id: artist.id)
+  begin
+    Artist.find_by!(name: song["song_artist"])
+  rescue
+    @artist = Artist.create(name: song["song_artist"])
+  else
+    @artist = Artist.find_by!(name: song["song_artist"])
+  end
+  Song.create(name: song["song_title"], artist_id: @artist.id, topboard_id: @top.id)
 end
