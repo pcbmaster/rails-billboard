0bc1d5491a5caa5505de12ea8c2d335c10a67447
Dirk Dobson <dirkvancouver@live.com>
2018-09-06 14:28:50 -0600

    Song index

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,7 @@
 class SongsController < ApplicationController
 #  before_action :set_song, only: [:show, :edit, :update, :destroy] this breaks everything?
   def index
-    
+    @songs = Song.all
   end
 
   def show
