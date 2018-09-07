731f3f3a7f762685bdf17875d7c05afa4a24e122
Jordan Rand <jorrand@gmail.com>
2018-09-07 15:22:14 -0600

    force user to sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,18 @@
 class ApplicationController < ActionController::Base
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
   protect_from_forgery
 
   helper_method :current_user
 
   private
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-  
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    end
 end
